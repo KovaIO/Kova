56b7d6320944f97d5486e940fcfd142010e7de28
Kevinrestrepoh <kevinrestrepoh@gmail.com>
2026-06-08 14:39:54 -0500

    feat: add workspace profiles selector

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -204,8 +204,10 @@ pub fn run() {
             if let Some(process) = app.get_webview_window("process") {
                 windows::attach_focus_hide(process);
             }
-
             if let Some(clippy) = app.get_webview_window("clipboard") {
+                windows::attach_focus_hide(clippy);
+            }
+            if let Some(clippy) = app.get_webview_window("profiles") {
                 windows::attach_focus_hide(clippy);
             }
 
